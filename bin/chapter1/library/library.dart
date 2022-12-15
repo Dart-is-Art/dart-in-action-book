37926a817c080399d64b14b2567775e05cae55e7
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-12-15 10:18:36 +0330

    didnt fully understand part and import. i should comeback and search

---
+++ bin/chapter1/library/library.dart
@@ -0,0 +1,7 @@
+/// the syntax in the early versions of dart was
+/// library "mylib"
+library mylib;
+/// in dart if you want to use top level functions you don't
+/// need to create classes and embed functions inside them.
+import '../OOP/class.dart';
+part '../OOP/class.dart';
