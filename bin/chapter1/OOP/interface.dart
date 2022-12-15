7f465deb8a758526eb0dde17d1f16fad5ef7563e
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-12-15 09:51:27 +0330

    factory constructors are practiced

--- bin/chapter1/OOP/interface.dart
+++ bin/chapter1/OOP/interface.dart
@@ -16,9 +16,40 @@ class Square implements Shape{
   }
 }
 
+/// there is a difference between factory constructors and generative constructors 
+/// in dart. interfaces usually do not have generative constructors because instance of
+/// interfaces is not going to be made so they can't be extended since having a
+/// at least one generative constructor is a prerequisite to be extendable. 
+abstract class IGreetable {
+  /// if you want to don't provide implementation of methods 
+  /// in a class, you have to make that class abstract.
+  String sayHello(String name);
+  factory IGreetable() {
+    /// the class that a factory return has to have implemented the parent class. 
+    /// in this case, Greeter has to have implemented IGreetable.
+    return Greeter();
+  }
+  /// interfaces which have factory constructors cannot have generative constructors
+}
+/// factory constructors are mostly useful in single implementation cases.
+class Greeter implements IGreetable{
+  @override
+  String sayHello(String name) {
+    return 'hello $name';
+  }
+  /// constructors do not need to be overridden since the interface has 
+  /// a factory constructor.
+  
+}
+/// abstract classes are conceptually equivalent to
+/// interfaces in java. However, normal classes have a hybrid identity,
+/// meaning, they can behave as a combination of classes and interfaces in java.
 void main() {
   Shape square = Square();
   Shape shape = Shape();
   shape.sayHello();
   square.sayHello();
+  
+  IGreetable iGreetable = IGreetable();
+  print(iGreetable.sayHello('ali'));
 }
